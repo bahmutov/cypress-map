a38da92541e157d0d4851727ea7fbd97ccb74f53
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-07 08:48:13 -0500

    working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,31 +4,9 @@ const getTexts = ($el) => {
   return Cypress._.map($el, 'innerText')
 }
 
-it.skip('confirms the list without retries', () => {
+it('confirms the list', () => {
   cy.visit('cypress/index.html')
   cy.get('.matching')
     .then(getTexts)
-    .should('deep.equal', ['first', 'third', 'fourth'])
-})
-
-// Cypress.Commands.addQuery('map', (fn) => {
-//   return (subject) => fn(subject)
-// })
-
-Cypress.Commands.addQuery('map', (fnOrProperty) => {
-  if (typeof fnOrProperty === 'string') {
-    return ($el) => Cypress._.map($el, fnOrProperty)
-  }
-  if (typeof fnOrProperty === 'function') {
-    return (subject) => fnOrProperty(subject)
-  }
-
-  throw new Error('Not sure how to map...')
-})
-
-it.only('confirms the test', () => {
-  cy.visit('cypress/index.html')
-  cy.get('.matching')
-    .map('innerText')
     .should('deep.equal', ['first', 'third', 'fourth'])
 })

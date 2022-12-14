a9fe6093206d027389911832408673b522a60b74
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-14 10:50:15 -0500

    update text spec

--- cypress/e2e/text.cy.js
+++ cypress/e2e/text.cy.js
@@ -1,7 +1,22 @@
-it('checks the text is one of the variants', () => {
+// confirm the first LI element has text
+// that is either 'first' or 'primo'
+
+it('checks the text is one of the variants: single should callback', () => {
+  cy.visit('cypress/index.html')
+  cy.get('#items li').should(($li) => {
+    expect($li.first().text()).to.be.oneOf(['first', 'primo'])
+  })
+})
+
+it('checks the text is one of the variants: regex', () => {
+  cy.visit('cypress/index.html')
+  cy.contains('#items li:first', /^(first|primo)$/)
+})
+
+it('checks the text is one of the variants: v12 queries', () => {
   cy.visit('cypress/index.html')
   cy.get('#items li')
     .first()
     .invoke('text')
-    .should('be.oneOf', ['first', 'second'])
+    .should('be.oneOf', ['first', 'primo'])
 })

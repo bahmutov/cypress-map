822a316d0b537b0140a40bd9b5ab8658a56728f4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-19 17:13:06 -0500

    add json data attibute example

---
+++ cypress/e2e/json-attribute/spec.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+// @ts-check
+import '../../../commands'
+
+// implementation similar to "Json data attribute" recipe
+// from https://glebbahmutov.com/cypress-examples
+
+it('compares the parsed data attribute object', () => {
+  cy.visit('cypress/e2e/json-attribute/index.html')
+  // grab the element's attribute "data-field"
+  // convert it into a JSON object
+  // and grab its "age" property -> should be equal 10
+  cy.get('#person')
+    .invoke('attr', 'data-field')
+    .apply(JSON.parse)
+    .its('age')
+    .should('equal', 10)
+})

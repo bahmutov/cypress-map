0a2e8c8bee1133a09cacc59edf9e6fe61de5f8a1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-19 16:56:31 -0500

    feat: add cy.table command

--- cypress/e2e/table.cy.js
+++ cypress/e2e/table.cy.js
@@ -4,14 +4,69 @@
 // import cypress-map plugin
 import '../../commands'
 
-describe('table', { viewportWidth: 400, viewportHeight: 400 }, () => {
-  it('checks the column of cells', () => {
+describe('table', { viewportWidth: 300, viewportHeight: 200 }, () => {
+  beforeEach(() => {
     cy.visit('cypress/table.html')
+  })
+
+  it('checks the column of cells', () => {
     cy.contains('table#people thead td:nth-child(2)', 'Age')
     cy.get('table#people tbody td:nth-child(2)')
       .should('have.length', 4)
       .map('innerText')
       .map(Number)
       .should('deep.equal', [20, 30, 28, 22])
+  })
+
+  it('gets the entire table', () => {
+    cy.get('table')
+      .table()
+      .should('deep.equal', [
+        ['Name', 'Age', 'Date (YYYY-MM-DD)'],
+        ['Dave', '20', '2023-12-23'],
+        ['Cary', '30', '2024-01-24'],
+        ['Joe', '28', '2022-02-25'],
+        ['Anna', '22', '2027-03-26'],
+      ])
+  })
+
+  it('gets the headings', () => {
+    cy.get('table')
+      .table(0, 0, 3, 1)
+      .its(0)
+      .print()
+      .should('deep.equal', ['Name', 'Age', 'Date (YYYY-MM-DD)'])
+  })
+
+  it('gets the headings row', () => {
+    cy.get('table')
+      .table(0, 0)
+      .its(0)
+      .print()
+      .should('deep.equal', ['Name', 'Age', 'Date (YYYY-MM-DD)'])
+  })
+
+  it('gets the first column', () => {
+    cy.get('table')
+      .table(0, 0, 1, 5)
+      .print()
+      .should('deep.equal', [['Name'], ['Dave'], ['Cary'], ['Joe'], ['Anna']])
+  })
+
+  it('gets the entire first column', () => {
+    cy.get('table')
+      .table(0, 0, 1)
+      .print()
+      .should('deep.equal', [['Name'], ['Dave'], ['Cary'], ['Joe'], ['Anna']])
+  })
+
+  it('gets a region of the table', () => {
+    cy.get('table')
+      .table(0, 2, 2, 2)
+      .print()
+      .should('deep.equal', [
+        ['Cary', '30'],
+        ['Joe', '28'],
+      ])
   })
 })

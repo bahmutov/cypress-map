dd41e8e0af063e77c73026ed88e38f749f954d03
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-06 19:21:52 -0500

    fix: return instance of array from cy.map (#23)

--- commands/map.js
+++ commands/map.js
@@ -2,13 +2,22 @@
 
 Cypress.Commands.addQuery('map', (fnOrProperty) => {
   const message =
-    typeof fnOrProperty === 'string' ? fnOrProperty : fnOrProperty.name
+    typeof fnOrProperty === 'string'
+      ? fnOrProperty
+      : fnOrProperty.name
   const log = Cypress.log({ name: 'map', message })
 
-  return ($el) =>
-    Cypress._.map($el, (item) =>
-      typeof fnOrProperty === 'string'
-        ? item[fnOrProperty]
-        : fnOrProperty(item),
-    )
+  return ($el) => {
+    // use a spread so that the result is an array
+    // and used the Array constructor in the current iframe
+    // so it passes the "instanceOf Array" assertion
+    const list = [
+      ...Cypress._.map($el, (item) =>
+        typeof fnOrProperty === 'string'
+          ? item[fnOrProperty]
+          : fnOrProperty(item),
+      ),
+    ]
+    return list
+  }
 })

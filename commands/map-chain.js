1ee1c7b0e3dfea759d386c36bcc712c889c8a541
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-08 20:58:56 -0500

    remove extra code

--- commands/map-chain.js
+++ commands/map-chain.js
@@ -10,36 +10,22 @@ Cypress.Commands.add(
     const results = []
 
     const produceValue = (k) => {
-      cy.wrap()
+      return cy
+        .wrap()
         .then(() => fn(list[k], k))
-        .then((r) => {
-          // if the function returned a Promise
-          // or "cy" chainable
-          if (typeof r === 'object' && 'then' in r) {
-            return r.then((result) => {
-              // console.log(k, list[k], result)
-              results.push(result)
-              if (k >= list.length - 1) {
-                // done
-              } else {
-                return produceValue(k + 1)
-              }
-            })
+        .then((result) => {
+          results.push(result)
+          if (k >= list.length - 1) {
+            // done
           } else {
-            // user function is plain
-            // console.log(k, list[k], r)
-            results.push(r)
-            if (k >= list.length - 1) {
-              // done
-            } else {
-              return produceValue(k + 1)
-            }
+            return produceValue(k + 1)
           }
         })
     }
 
     // make sure we put the possible promises into the command chain
-    cy.wrap(results, { log: false })
+    return cy
+      .wrap(results, { log: false })
       .then(() => produceValue(0))
       .then(() => results)
   },

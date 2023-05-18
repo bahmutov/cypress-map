409a61385a7d4d336a25f76e1b26cd345972b1d1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-05-18 11:37:18 -0400

    fix: sample yields jQuery

--- commands/sample.js
+++ commands/sample.js
@@ -7,7 +7,10 @@ registerQuery('sample', () => {
 
   return (subject) => {
     if (Cypress.dom.isJquery(subject)) {
-      return Cypress._.sample(subject.toArray())
+      const randomElement = Cypress._.sample(subject.toArray())
+      // wrap into jQuery object so other commands
+      // can be attached, like cy.click
+      return Cypress.$(randomElement)
     }
 
     return Cypress._.sample(subject)

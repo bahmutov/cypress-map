fc07eade8d93828ea45f6c1dc05f60f1fc40c446
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-08 11:49:45 -0500

    fix: remove the old commands folder

--- commands/to-plain-object.js
+++
@@ -1,7 +0,0 @@
-var registerQuery = require('./utils').registerQuery;
-registerQuery('toPlainObject', function () {
-    var log = Cypress.log({ name: 'toPlainObject' });
-    return function (subject) {
-        return JSON.parse(JSON.stringify(subject));
-    };
-});
--- commands/version-check.js
+++
@@ -1,4 +0,0 @@
-var major = Cypress.version.split('.').map(Number)[0];
-if (major < 12) {
-    throw new Error("cypress-map requires Cypress version >= 12");
-}
